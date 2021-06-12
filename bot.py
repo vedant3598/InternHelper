1045ccb4f1fa23ca7b0c636030ca21dc9767f235
Vedant Shah <vedant18@hotmail.com>
2021-06-11 23:17:21 -0400

    Adding database and save, apply commands

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import sqlite3
 from discord.ext import commands
 import discord.ext.commands.errors
 
@@ -7,6 +8,8 @@ intents = discord.Intents.default()
 intents.members = True
 # Discord Token for intern_helper_bot
 TOKEN = 'DISCORD_TOKEN'
+# adding connection to internships.db
+connection = sqlite3.connect("internships.db")
 
 intern_helper_bot = commands.Bot(command_prefix='$', intents=intents)
 
@@ -30,6 +33,17 @@ async def server_info(ctx):
         server.add_field(name=name, value=value, inline=inline)
 
     await ctx.send(embed = server)
+
+# saves job listing in the database 
+@intern_helper_bot.command(name='save <company_name> <position> <notes> <location_optional>')
+async def server_info(ctx):
+    cursor = connection.cursor()
+
+
+# adds apply tag to an existing job listing in the database; if job listing does not exist, adds it with apply tag
+@intern_helper_bot.command(name='apply <company_name> <position> <notes> <location_optional>')
+async def server_info(ctx):
+    cursor = connection.cursor()
 
 
 intern_helper_bot.run(TOKEN)
