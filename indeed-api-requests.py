fdbb6fb9e04bc7d0ceb504288c627a296c6dda39
Vedant Shah <vedant18@hotmail.com>
2021-08-04 21:13:46 -0400

    Updated To Do list

--- indeed-api-requests.py
+++ indeed-api-requests.py
@@ -5,9 +5,11 @@ import socket
 # Initiating an IndeedClient object
 client = IndeedClient(publisher = )
 
+# Client IP address to get IP address of user (required for 'userip' of parameter request to Indeed API)
 client_hostname = socket.gethostname()
 client_ip_address = socket.gethostbyname(client_hostname)
 
+# 
 def position_location(location: str, position: str):
     parameters = {
         'q': "{position}",
@@ -32,3 +34,4 @@ def position(position: str):
     }
 
     search_response = client.search(**parameters)
+
